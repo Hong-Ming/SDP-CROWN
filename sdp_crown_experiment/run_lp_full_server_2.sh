9d867b751f73a0c776643723e54fcbe1917a6a04
Hong-Ming <h15731573@gmail.com>
2025-05-25 06:02:05 -0500

    initial commit

--- sdp_crown_experiment/run_lp_full_server_2.sh
+++ sdp_crown_experiment/run_lp_full_server_2.sh
@@ -4,7 +4,7 @@ python lp_full.py --model mnist_convsmall --radius 0.3
 # python lp_full.py --model mnist_convlarge --radius 0.3
 
 # CIRAR10
-python lp_full.py --model cifar10_cnn_a --radius 24/255
+# python lp_full.py --model cifar10_cnn_a --radius 24/255
 # python lp_full.py --model cifar10_cnn_b --radius 24/255
 # python lp_full.py --model cifar10_cnn_c --radius 24/255
 # python lp_full.py --model cifar10_convbase --radius 24/255
