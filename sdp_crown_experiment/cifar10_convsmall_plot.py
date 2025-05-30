eb3d1269f6dfc13358ac6c26e0c2d6ac62c68d8a
Hong-Ming <h15731573@gmail.com>
2025-05-30 03:56:26 -0500

    add hplot code

--- sdp_crown_experiment/cifar10_convsmall_plot.py
+++ sdp_crown_experiment/cifar10_convsmall_plot.py
@@ -49,7 +49,7 @@ for radius in radii:
     args.radius = radius
     model, dataset, labels, radius_rescale, classes = load_model_and_dataset(args, device)
     attack_pgd(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)  
-    verified_sdp_crown(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)  
-    verified_alpha_crown(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)
-    verified_lipnaive(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)
+    # verified_sdp_crown(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)  
+    # verified_alpha_crown(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)
+    # verified_lipnaive(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)
     # verified_lp_all(dataset = dataset, labels = labels, model = model, radius = radius_rescale, clean_output = correct_indices, device = device, classes = classes, args = args)
